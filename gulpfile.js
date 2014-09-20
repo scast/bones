b78bfad5ae63f8ded5a6a12c48b6eaf7cb55d907
Simón Castillo <simoncastillo@gmail.com>
2014-09-20 19:00:37 -0430

    Adding php live reload.

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,22 @@ var lr = require('tiny-lr');
 var notify = require('gulp-notify');
 var livereload = require('gulp-livereload');
 var changed = require('gulp-changed');
+var minifyCss = require('gulp-minify-css')
 var server = lr();
 
 gulp.task('styles', function() {
     gulp.src('./library/scss/**/*.scss')
         .pipe(sass())
+        .pipe(minifyCss({ keepSpecialComments: 1 }))
         .pipe(gulp.dest('./library/css'))
         .pipe(livereload(server))
         .pipe(notify("Done building bones styles."));
 });
+
+gulp.task('livereload-php', function() {
+    gulp.src('./**/*.php')
+        .pipe(livereload(server));
+})
 
 gulp.task('watch', function() {
 
@@ -21,6 +28,7 @@ gulp.task('watch', function() {
             return console.log(err);
         }
         gulp.watch('./library/scss/**/*.scss', ['styles']);
+        gulp.watch('./**/*.php', ['livereload-php']);
     });
 
 
